9a203886f332702d5a7d30c8e0a492b0b4e8b484
RubensPS <rubens_p_souza@hotmail.com>
2022-05-25 14:45:10 -0300

    Initial commit

---
+++ src/main/java/com/letscode/ecsalesapi/EcSalesApiApplication.java
@@ -0,0 +1,13 @@
+package com.letscode.ecsalesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcSalesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcSalesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/letscode/ecsalesapi/EcSalesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letscode.ecsalesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcSalesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
